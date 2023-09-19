16c3ac424b63cf528a57b6b7d201db507e859fc0
Rene Probst <rene.probst@bzz.ch>
2023-09-19 16:31:49 +0200

    prepared for the lessons

--- customer.py
+++ customer.py
@@ -1,5 +1,4 @@
 from person import Person
-from employee import Employee
 
 class Customer(Person):
     '''
@@ -11,44 +10,3 @@ class Customer(Person):
     die Ausgabe der Oberklasse Person ausgeführt.
     '''
 
-    def __init__(self, name, age, client_advisor):
-        '''
-        Initlaisiert ein Kunden-Objekt mit einer einseitigen Beziehung zu Mitarbeiter
-        :param name: Name des Kunden
-        :param age: Alter des Kunden
-        :param client_advisor: Referenz zum Kundenbetreuer
-        '''
-        super().__init__(name, age)
-        self._client_advisor = client_advisor
-
-    @property
-    def client_advisor(self):
-        '''
-        Liefert die Referenz zum Kundenberater
-        :return: Referenz des Kundenberaters
-        '''
-        return self._client_advisor
-
-    @client_advisor.setter
-    def client_advisor(self, client_advisor):
-        '''
-        Sett die Referenz zum Kundenberater
-        :param client_advisor: Referenzauf Kundenberater
-        '''
-        self._client_advisor = client_advisor
-
-    def print(self):
-        '''
-        Gibt alle Information einer Mitarbeiterin am Stdout aus.
-        Dazu muss die entsprechende Methode der Oberklasse auch aufgerufen werden.
-        '''
-        super().print()
-        print(f'\t\tKundenberater: {self._client_advisor.name}\t{self._client_advisor.phone}')
-
-
-#Test
-if __name__ == '__main__':
-    e = Employee('Max', 44, '455.45.T', '099-345-332-123')
-    c = Customer('Pia', 30, e)
-    c.address = 'Oberdorf'
-    c.print()
--- regular_customer.py
+++ regular_customer.py
@@ -1,5 +1,5 @@
 from customer import Customer
-from employee import Employee
+
 
 class RegularCustomer(Customer):
    '''
@@ -11,45 +11,3 @@ class RegularCustomer(Customer):
     die Ausgabe der Oberklasse Customer ausgeführt.
    '''
 
-   def __init__(self, name, age, client_advisor):
-      '''
-      Initlaisiert ein Kunden-Objekt mit einer einseitigen Beziehung zu Mitarbeiter
-      :param name: Name des Kunden
-      :param age: Alter des Kunden
-      :param client_advisor: Referenz zum Kundenbetreuer
-      '''
-      super().__init__(name, age, client_advisor)
-
-   @property
-   def discount(self):
-      '''
-      Liefert den Rabatt des Stammkunden
-      :return: Rabatt des Stammkunden
-      '''
-      return self._discount
-
-   @discount.setter
-   def discount(self, discount):
-      '''
-      Legt den rabatt des Stammkunden fest.
-      :param discount: Rabatt des Stammkunden
-      '''
-      self._discount = discount
-
-
-   def print(self):
-      '''
-      Gibt alle Information einer Mitarbeiterin am Stdout aus.
-      Dazu muss die entsprechende Methode der Oberklasse auch aufgerufen werden.
-      '''
-      super().print()
-      print(f'\t\tStammkunde mit {self._discount}% Rabatt')
-
-
-#Test
-if __name__ == '__main__':
-   e = Employee('Max', 2000, '455.45.T', '099-345-332-123')
-   r = RegularCustomer('Pia', 1980, e)
-   r.address  = 'Oberdorf'
-   r.discount = 2.4
-   r.print()
