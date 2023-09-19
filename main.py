815877773fa8ef508ae05f0e5fb49f8b4492c9f8
Rene Probst <rene.probst@bzz.ch>
2023-09-19 16:26:15 +0200

    initial commit

--- main.py
+++ main.py
@@ -1,12 +1,28 @@
-def calculate():
-    """
-    calculates the total
-    :return: None
-    """
-    price = 15.75
-    quantity = 3  # TODO change quantity to 5
-    print(f'Total: {price * quantity}')
+from person import Person
+from employee import Employee
+from customer import Customer
+from regular_customer import RegularCustomer
 
-
-if __name__ == '__main__':
-    calculate()
+if __name__ == "__main__":
+    # eine allgemeine Person erstellen
+    p = Person("Max", 25)
+    p.adress = "Musterdorf"
+    p.print()
+    print("--------------------------------------------")
+    # Einen Mitarbeiter erstellen
+    e = Employee("Joel", 35, "723-123", "01 03 04 05")
+    e.adress = "Weltdorf"
+    e.salary = 6000.00
+    e.print()
+    print("--------------------------------------------")
+    # einen Kunden erstellen
+    c = Customer("Moritz", 45, e)
+    c.adress = "Oberdorf"
+    c.print()
+    print("--------------------------------------------")
+    # eine Stammkundin erstellen
+    r = RegularCustomer("Pia", 25, e)
+    r.adress = "Oberdorf"
+    r.discount = 2.4
+    r.print()
+    print("--------------------------------------------")
